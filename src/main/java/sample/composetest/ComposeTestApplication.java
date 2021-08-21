f1b7e82ebdf4f4fb2e94790e865fa21d5c700590
9deok <butterflynine20@gmail.com>
2021-08-21 22:47:08 +0900

    first commit

---
+++ src/main/java/sample/composetest/ComposeTestApplication.java
@@ -0,0 +1,13 @@
+package sample.composetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComposeTestApplication.class, args);
+	}
+
+}
